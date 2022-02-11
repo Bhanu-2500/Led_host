b179fc06e61ab26ed7c7047139bc1dc5b380cc0e
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2022-02-11 17:59:00 +0530

    eete

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT 
 
-var ledStatus= new Boolean();
+let ledStatus= new Boolean();
 ledStatus=0;
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.post('/master', function (req, res) {
+app.post('/', function (req, res) {
    ledStatus=req.body.status;
    res.send(ledStatus);
 
@@ -19,6 +19,4 @@ app.get('/slave', function (req, res) {
    res.send(ledStatus);
 });
 
-app.listen(port, () => {
-   console.log(`App listening at http://localhost:${port}`)
- });
+app.listen(port);
