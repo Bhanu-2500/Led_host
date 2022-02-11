1528a50521603539bc470974d4079ea62ef9b410
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2022-02-11 17:40:10 +0530

    second edit

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require('express');
 const app = express();
 
 var ledStatus= new Boolean();
+ledStatus=0;
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.post('/master', function (req, res) {
    ledStatus=req.body.status;
    res.send(ledStatus);
+   
 });
 
 app.get('/slave', function (req, res) {
