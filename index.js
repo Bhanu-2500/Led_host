0ff89f4a7c91c4ab90c631374819416e138bcef3
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2022-02-11 17:13:00 +0530

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+var ledStatus= new Boolean();
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.post('/master', function (req, res) {
+   ledStatus=req.body.status;
+   res.send(ledStatus);
+});
+
+app.get('/slave', function (req, res) {
+   res.send(ledStatus);
+});
+app.listen(8081);
